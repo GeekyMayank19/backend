bc98d2e167a1cee6914e0366daf859d8805d4511
Mayank Sharma <46962063+GeekyMayank19@users.noreply.github.com>
2021-11-17 21:16:46 +0530

    Update index.js

--- socialapp/index.js
+++ socialapp/index.js
@@ -5,6 +5,9 @@ app.get("/",(req,res)=>{
 res.send("that's i already know")
 })
 
+app.get("/v1/instagram",()=>{
+res.status(200).json
+})
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`);
 })
