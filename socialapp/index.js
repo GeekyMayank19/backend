c59c9fb8cecc49252e425db37c22526adbb55b50
Mayank Sharma <46962063+GeekyMayank19@users.noreply.github.com>
2021-11-18 12:32:17 +0530

    params

--- socialapp/index.js
+++ socialapp/index.js
@@ -1,13 +1,53 @@
+
 const express = require("express")
+var format = require('date-format');
+format.asString();
+
 const app = express();
 const PORT = 4000 || process.env.PORT;
+
+
+
 app.get("/",(req,res)=>{
 res.send("that's i already know")
 })
 
-app.get("/v1/instagram",()=>{
-res.status(200).json
+app.get("/api/v1/:tokenfuckyou",(req,res)=>{
+    res.status(200).json({param: req.params})
 })
+
+app.get("/v1/instagram",(req,res)=>{
+    const instaSocial = {
+        userName: "kakarot",
+        follower: 56,
+        follow: 60,
+        date : format.asString('"DAY"- dd/MM/yy "Time"- hh:mm:ss', new Date())
+    }
+res.status(200).json(instaSocial)
+})
+
+app.get("/v1/facebook",(req,res)=>{
+    const instaSocial = {
+        userName: "kakarot",
+        follower: 56,
+        follow: 60,
+        date : Date.now()
+    }
+res.status(200).json(instaSocial)
+})
+
+
+app.get("/v1/linkedin",(req,res)=>{
+        const instaSocial = {
+            userName: "kakarot",
+            follower: 56,
+            follow: 60,
+            date : Date.now()
+        }
+res.status(200).json(instaSocial)
+})
+
+
 app.listen(PORT,()=>{
     console.log(`server is running on port ${PORT}`);
 })
